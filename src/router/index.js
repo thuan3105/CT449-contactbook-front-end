caa2212cf89250b3c1a4790f28fb704068061af6
thuan3105 <thuannguyen31052001@gmail.com>
2023-03-24 01:24:11 +0700

    them tinh nang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,34 @@
-import {createRouter,createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "ContactBook",
-        component: ContactBook,
-    },
-    {
-        path: "/contacts/:id",
-        name: "EditContact",
-        component: () => import("@/views/ContactEdit.vue"),
-        props: true // Truyền các biến trong $route.params vào làm props
-    },
-    {
-        path: "/add",
-        name: "AddContact",
-        component: () => import("@/views/ContactAdd.vue"),
-        props: true // Truyền các biến trong $route.params vào làm props
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        name: "NotFound",
-        component: () => import("@/views/NotFound.vue"),
-        },
+  {
+    path: "/",
+    name: "ContactBook",
+    component: ContactBook,
+  },
+  {
+    path: "/contacts/:id",
+    name: "EditContact",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/add",
+    name: "AddContact",
+    component: () => import("@/views/ContactAdd.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
